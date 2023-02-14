6f80cf6e32c672a7b721d8922987cf3ad9d0e0f0
FallenAngel888 <ricardojortigoza10@gmail.com>
2023-02-14 14:56:36 -0400

    asd

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+ public class Main {
+     public static void main(String[] args) {
+         coche micoche = new coche();
+         micoche.agregarp();
+
+         micoche.mostrarP();
+
+
+
+     }
+ }
---
+++ src/coche.java
@@ -0,0 +1,14 @@
+public class coche {
+int numerop=4;
+
+    public void agregarp() {
+        numerop++;
+    }
+
+    public void mostrarP() {
+        System.out.println("la cantidad de puerta es:"+numerop);
+
+    }
+}
+
+
